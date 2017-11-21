4677f880b6a3cb128b73a55583d3ac68e360203e
40201144 <fraserrae117@gmail.com>
2017-11-21 19:01:32 +0000

    Just need comments and streamlining... and report

--- test.py
+++ test.py
@@ -8,8 +8,38 @@ def root():
 
 #Defines route to the 'Main menu' for this project
 @app.route('/main/')
-def types():
+def main():
     return render_template("templates.html")
+
+#Defines route to Ekans 
+@app.route('/ekans')
+def ekans():
+    return render_template("ekans.html")
+
+#Defines route to Magnemite 
+@app.route('/magnemite')
+def mag():
+    return render_template("magnemite.html")
+
+#Defines route to Trumbeak 
+@app.route('/trumbeak')
+def tru():
+    return render_template("trumbeak.html")
+
+#Defines route to Rockruff 
+@app.route('/rockruff')
+def rock():
+    return render_template("rockruff.html")
+
+#Defines route to Torracat 
+@app.route('/torracat')
+def torr():
+    return render_template("torracat.html")
+
+#Defines route to Gastly 
+@app.route('/gastly')
+def gast():
+    return render_template("gastly.html")
 
 #Puts the project online
 if __name__ == "__main__":
